0029d59873bd028a7f188661b4959c4675da45ef
Elijah Tabizon <elijahtabizon25@gmail.com>
2024-07-02 23:11:11 -0700

    close and open modal

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,17 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+const profileCloseButton = document.querySelector("#profile-close-button");
+const profileCloseModal = document.querySelector("#profile-close-modal");
+
+profileCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
